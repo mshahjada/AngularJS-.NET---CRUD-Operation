550ee04138e3f9f521a1917eecfa28281f5eb4fe
mshahjada <sagor.cse09@gmail.com>
2017-08-06 00:32:57 +0600

    CRUD Operation By AngularJS & .NET
    It may help you primarily how to work with angularJS & .NET.
    Here a custom factory (customerFac) is written to minimize code.
    You will may use this factory where it is needed & must have to
    declare because of it's functional dependency.

---
+++ Angular-CRUD/AngularBasic/Controllers/HomeController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AngularBasic.Models;
+
+namespace AngularBasic.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        
+
+        //private void DataKeep(List<Customer> customer)
+        //{
+        //    if(Session["Data"] == null)
+        //    {
+        //        Session["Data"] = DataSet.GetAllCustomer();
+        //    }
+        //    else
+        //    {
+        //        Session["Data"] = customer;
+
+        //    }
+        //}
+
+        public ActionResult Index()
+        {
+            DataSet.Initialize();
+            ViewBag.Customers = DataSet.GetAllCustomer();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public JsonResult Get(int id)
+        {
+            Customer customer = new Customer();
+            try
+            {
+               var result = DataSet.GetAllCustomer().Where(x => x.CustomerId == id).ToList();
+                if (result.Any())
+                    customer = result.First();
+            }
+            catch (Exception e)
+            {
+                Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                Response.StatusDescription = e.Message.ToString();
+            }
+            return Json(customer, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Save(Customer customer)
+        {
+            try
+            {
+                if (customer.CustomerId <=0 )
+                    throw new Exception("Invalid Id.");
+                else if (customer.CustomerName.Trim() == "")
+                    throw new Exception("Customer Name Required.");
+                else if (customer.Address.Trim() == "")
+                    throw new Exception("Customer Address Required.");
+
+                DataSet.AddCustomer(customer);
+
+                var x = DataSet.GetAllCustomer();
+                x.Add(customer);
+
+                var t = Session["Data"];
+                Session["Data"] = x;
+
+                
+            }
+            catch(Exception e)
+            {
+                Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                Response.StatusDescription = e.Message.ToString();
+            }
+            return Json(customer, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPut]
+        public JsonResult Edit(Customer customer)
+        {
+            try
+            {
+                if (!DataSet.GetAllCustomer().Where(x => x.CustomerId == customer.CustomerId).Any())
+                    throw new Exception("Invalid Customer, Unable to Edit.");
+                else if (customer.CustomerName.Trim() == "")
+                    throw new Exception("Customer Name Required.");
+                else if (customer.Address.Trim() == "")
+                    throw new Exception("Customer Address Required.");
+
+                DataSet.EditCustomer(customer);
+            }
+            catch (Exception e)
+            {
+                Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                Response.StatusDescription = e.Message.ToString();
+            }
+            return Json(customer, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpDelete]
+        public JsonResult Delete(Customer customer)
+        {
+            bool remove = false;
+            try
+            {
+                if (!DataSet.GetAllCustomer().Where(x=>x.CustomerId== customer.CustomerId).Any())
+                    throw new Exception("Invalid Customer, Unable to Delete.");
+
+                DataSet.DeleteCustomer(customer);
+                remove = true;
+            }
+            catch (Exception e)
+            {
+                Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                Response.StatusDescription = e.Message.ToString();
+            }
+            return Json(remove, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ Angular-CRUD/AngularBasic/Models/Customer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularBasic.Models
+{
+    public class Customer
+    {
+        public Customer()
+        {
+            CustomerId = 0;
+            CustomerName = string.Empty;
+            Address = string.Empty;
+            ContactNo = string.Empty;
+            Remarks = string.Empty;
+        }
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public string ContactNo { get; set; }
+        public string Remarks { get; set; }
+    }
+
+    public class DataSet
+    {
+        public static void Initialize()
+        {
+            customers = new List<Customer>();
+            customers.Add(new Customer { CustomerId = 1, CustomerName = "Kamal", Address = "Bangladesh", ContactNo = "123", Remarks = "N/a" });
+            customers.Add(new Customer { CustomerId = 2, CustomerName = "Toru", Address = "Japan", ContactNo = "123", Remarks = "N/a" });
+            customers.Add(new Customer { CustomerId = 3, CustomerName = "Rased", Address = "Bangladesh", ContactNo = "123", Remarks = "N/a" });
+            customers.Add(new Customer { CustomerId = 4, CustomerName = "Jiko", Address = "England", ContactNo = "123", Remarks = "N/a" });
+            customers.Add(new Customer { CustomerId = 5, CustomerName = "Natasa", Address = "Germany", ContactNo = "123", Remarks = "N/a" });
+
+        }
+        private static List<Customer> customers { get; set; }
+
+
+        public static List<Customer> AddCustomer(Customer customer)
+        {
+            customers.Add(customer);
+            return customers;
+        }
+
+        public static List<Customer> EditCustomer(Customer customer)
+        {
+            customers.RemoveAll(x=>x.CustomerId == customer.CustomerId);
+            customers.Add(customer);
+            return customers;
+        }
+
+        public static List<Customer> DeleteCustomer(Customer customer)
+        {
+            customers.RemoveAll(x => x.CustomerId == customer.CustomerId);
+            return customers;
+        }
+
+        public static List<Customer> GetAllCustomer()
+        {
+            return customers;
+        }
+    }
+}
